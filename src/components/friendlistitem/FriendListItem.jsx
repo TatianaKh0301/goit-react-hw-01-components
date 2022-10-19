f30bdf4d654006d6d439b853fa6cec57bdbe8e56
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-10-19 17:15:19 +0300

    update

--- src/components/friendlistitem/FriendListItem.jsx
+++ src/components/friendlistitem/FriendListItem.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 const FriendListItem = ( {avatar, name, isOnline } ) => {
     return (
         <div>
-            <span class="status">{isOnline}</span>
-            <img class="avatar" src={avatar} alt={name} width="48" />
-            <p class="name">{name}</p>
+            <span className="status">{isOnline}</span>
+            <img className="avatar" src={avatar} alt={name} width="48" />
+            <p className="name">{name}</p>
         </div>
     );
 }
 
 FriendListItem.propTypes = {
     avatar: PropTypes.string.isRequired,
-    name: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
 }
 
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -38,13 +38,11 @@ Profile.propTypes = {
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    stats: PropTypes.arrayOf(
-        PropTypes.shape({
+    stats: PropTypes.shape({
             followers: PropTypes.number.isRequired,
             views: PropTypes.number.isRequired,
             likes: PropTypes.number.isRequired,
         }).isRequired,
-    ),
 }
 
 export default Profile;
