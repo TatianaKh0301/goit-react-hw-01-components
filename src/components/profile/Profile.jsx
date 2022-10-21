c45f7a9372b481770a8a6f287fdae72113457cae
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-10-21 17:34:20 +0300

    finish Profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -10,23 +10,31 @@ const Profile = ({username, tag, location, avatar, stats}) => {
                     <img src={avatar} alt={username}
                         className={css.avatar} 
                     />
-                    <p className="name">{username}</p>
-                    <p className="tag">{tag}</p>
-                    <p className="location">{location}</p>
+                    <div className={css.textwrap}>
+                        <p className={css.name}>{username}</p>
+                        <p className={css.tag}>{`@${tag}`}</p>
+                        <p className={css.location}>{location}</p>
+                    </div>                    
                 </div>
 
-                <ul className="stats">
-                    <li>
-                        <span className="label">Followers</span>
-                        <span className="quantity">{followers}</span>
+                <ul className={css.stats}>
+                    <li className={css.statsitem}>
+                        <div className={css.spanwrap}>
+                            <span className={css.label}>Followers</span>
+                            <span className={css.quantity}>{followers}</span>
+                        </div>                        
                     </li>
-                    <li>
-                        <span className="label">Views</span>
-                        <span className="quantity">{views}</span>
+                    <li className={css.statsitem}>
+                        <div className={css.spanwrap}>
+                            <span className={css.label}>Views</span>
+                            <span className={css.quantity}>{views}</span>
+                        </div>                        
                     </li>
-                    <li>
-                        <span className="label">Likes</span>
-                        <span className="quantity">{likes}</span>
+                    <li className={css.statsitem}>
+                        <div className={css.spanwrap}>
+                            <span className={css.label}>Likes</span>
+                            <span className={css.quantity}>{likes}</span>
+                        </div>                        
                     </li>
                 </ul>
             </div>
