ae58fac246fa7ac6da2b29a7d7cfcbe2aeff2767
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-10-23 06:20:17 +0300

    delete FriendList, FriendListItem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
 import user from 'components/user.json';
 import data from 'components/data.json';
-import friends from 'components/friends.json';
+// import friends from 'components/friends.json';
 import transactions from 'components/transactions.json'
 import Profile from 'components/profile/Profile';
 import Statistics from 'components/statistics/Statistics';
-import FriendList from 'components/friendList/FriendList';
 import TransactionHistory from 'components/transactionHistory/TransactionHistory';
 
 export const App = () => {
@@ -21,10 +20,6 @@ export const App = () => {
 
       <Statistics 
         stats={data}
-      />
-
-      <FriendList 
-        friends={friends} 
       />
 
       <TransactionHistory 
