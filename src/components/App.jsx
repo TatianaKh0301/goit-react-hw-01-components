18e12b35717a844df76257c254268162421f2d83
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-10-23 06:14:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import friends from 'components/friends.json';
 import transactions from 'components/transactions.json'
 import Profile from 'components/profile/Profile';
 import Statistics from 'components/statistics/Statistics';
-import FriendList from 'components/friendList/FriendList';
+import FriendList from './friendList/FriendList';
 import TransactionHistory from 'components/transactionHistory/TransactionHistory';
 
 export const App = () => {
