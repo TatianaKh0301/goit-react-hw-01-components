914b593ffff7017aafb3f94895e18f5c9f6a790f
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-10-23 06:24:00 +0300

    delete Statistics, StatisticItem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import user from 'components/user.json';
-import data from 'components/data.json';
+// import data from 'components/data.json';
 // import friends from 'components/friends.json';
 import transactions from 'components/transactions.json'
 import Profile from 'components/profile/Profile';
-import Statistics from 'components/statistics/Statistics';
+
 import TransactionHistory from 'components/transactionHistory/TransactionHistory';
 
 export const App = () => {
@@ -18,9 +18,6 @@ export const App = () => {
         stats={user.stats}
       />
 
-      <Statistics 
-        stats={data}
-      />
 
       <TransactionHistory 
         items = {transactions} 
